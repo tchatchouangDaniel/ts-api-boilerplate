0f382af9b109f4cbefd56687373aec9a94db6bd6
Daniel <tchatchouang.ouokam@gmail.com>
2022-01-19 05:47:09 +0100

    add missing types definition

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,25 @@
 /* eslint-disable no-console */
+enum Love {
+  Yes = 'yes',
+  No = 'no',
+}
+
 interface Identity {
   name: string;
   age: number;
   job: string;
+  loveNetflix: Love;
 }
 
 const identity: Identity = {
   name: 'Daniel',
   age: 22,
   job: 'Student',
+  loveNetflix: Love.Yes,
 };
 
 console.log(
-  `My name is ${identity.name}, i am ${identity.age} years old and i am a ${identity.job}`
+  `Me : My name is ${identity.name}, i am ${identity.age} years old and i am a ${identity.job}.\n
+  Someone : Do you love netflix ? 
+  Me : ${identity.loveNetflix}`
 );
